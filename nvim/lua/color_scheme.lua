7ee85f1cad2feab4943707191dd48f332396f259
5ohue <86558263+5ohue@users.noreply.github.com>
2024-05-04 13:36:07 +0300

    Neovim: Add command to change nova cs palette

--- nvim/lua/color_scheme.lua
+++ nvim/lua/color_scheme.lua
@@ -1,15 +1,33 @@
 -- Use pretty colors
 vim.opt.termguicolors = true
 
--- Load custom palette
-local palette = require('palettes.darktooth')
-require('nova_cs_theme_example').set_palette(palette)
+-- Function to change nova cs theme
+function set_colorscheme(palette_name)
+    local palette = require('palettes.' .. palette_name)
+
+    require('nova_cs_theme_example').set_palette(palette)
+
+    -- Set colorscheme
+    vim.cmd.colorscheme 'nova_cs_theme_example'
+
+    -- Set lualine colorscheme
+    require('lualine').setup { options = { theme = require('nova_cs_theme_example').lualine } }
+end
+-- ':NovaCS darktooth' to change nova cs palette
+vim.api.nvim_create_user_command(
+    'NovaCS',
+    function(args)
+        set_colorscheme(args['fargs'][1])
+    end,
+    {
+        nargs = 1
+    }
+)
 
 -- Set colorscheme
-vim.cmd.colorscheme 'nova_cs_theme_example'
+set_colorscheme('darktooth')
 
--- Set lualine colorscheme
-require('lualine').setup { options = { theme = require('nova_cs_theme_example').lualine } }
+
 
 -- Lsp's semantic tokens for readonly and static have the same priority, which
 -- makes the colors inconsistent. Doing the following fixes this.
@@ -31,3 +49,5 @@ vim.api.nvim_create_autocmd('LspTokenUpdate', {
         end
     end,
 })
+
+--
